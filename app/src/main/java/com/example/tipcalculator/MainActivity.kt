5b68a42ee84dcc7d6e36d7e26f45916cadd319df
Zyanik <106106326+geperez8@users.noreply.github.com>
2023-10-09 18:44:11 -0500

    Finalized Build

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -11,36 +11,68 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tipcalculator.ui.theme.TipCalculatorTheme
-
+import androidx.compose.ui.viewinterop.AndroidView
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.example.tipcalculator.R
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            TipCalculatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.layout)
+
+        val calculateButton = findViewById<Button>(R.id.CalculateButton)
+        calculateButton?.setOnClickListener {
+            showResult()
         }
+    }
+
+    private fun calculateResult(): Double {
+        val mealPriceNumber = findViewById<EditText>(R.id.MealPriceInput)
+        val tipPercentageNumber = findViewById<EditText>(R.id.TipAmountInput)
+
+        val mealPrice = mealPriceNumber?.text.toString().toDouble()
+
+        val tipPercentage = tipPercentageNumber?.text.toString().toDouble()
+
+        return mealPrice * (tipPercentage/100)
+    }
+
+    private fun showResult() {
+        val tipAmount = calculateResult()
+
+        val tipDisplay = findViewById<TextView>(R.id.Tip)
+
+        val partySize = findViewById<TextView>(R.id.PartySize).text.toString().toDouble()
+
+        val resultTip = ("%.2f".format(tipAmount/partySize))
+
+        tipDisplay.text = "Tip: $resultTip"
+
+        val mealPriceNumber = findViewById<EditText>(R.id.MealPriceInput)
+
+        val mealPrice = mealPriceNumber?.text.toString().toDouble()
+
+        val totalDisplay = findViewById<TextView>(R.id.Total)
+
+        val resultTotal = (tipAmount + mealPrice) / partySize
+
+        totalDisplay.text = "Total: %.2f".format(resultTotal)
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MyXmlLayout() {
+    AndroidView(
+        factory = { context ->
+            // Inflate your XML layout
+            LayoutInflater.from(context).inflate(R.layout.layout, null, false)
+        }
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TipCalculatorTheme {
-        Greeting("Android")
-    }
-}
+
+
+
